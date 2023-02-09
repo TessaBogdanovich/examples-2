1d53d6e3ddce9d90d892c8c65f359bc529e2f6de
Tessa Bogdanovich <surinastacey@gmail.com>
2023-02-09 14:44:41 +0300

    Add Example007

---
+++ Example007_Magic/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.Set.Cursor.Position(10,4);
+Console.WriteLine("+");
+
+int xa = 1;
+int ya = 1;
+int xb = 1;
+int yb = 30;
+int xc = 40;
+int yc = 30;
+Console.Set.Cursor.Position(xa,ya);
+Console.WriteLine ("+");
+
+Console.Set.Cursor.Position(xb,yb);
+Console.WriteLine ("+");
+
+Console.Set.Cursor.Position(xc,yc);
+Console.WriteLine ("+");
