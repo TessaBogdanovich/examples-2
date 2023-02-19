279078e234e4ce527c9f0790cb8986e4c4f2b739
Tessa Bogdanovich <surinastacey@gmail.com>
2023-02-19 13:53:29 +0300

    new example

---
+++ Example13_For/Program.cs
@@ -0,0 +1,18 @@
+﻿//string Method4(int count, string text)
+//{
+    //string result = String.Empty;
+    //for (int i = 0; i<count; i++)
+    //{
+        //result = result + text;
+    //}
+    //return result;
+//}
+//string res = Method4(10, "g");
+//Console.WriteLine(res);
+
+for(int i = 2; i<10; i++)
+{for (int j = 2; j <= 10; j++)
+{Console.WriteLine($"{i}*{j} = {i}*{j}");
+}
+System.Console.WriteLine();
+}
