c3da81882921c15a1ca67f2aea416d376f433777
Tessa Bogdanovich <surinastacey@gmail.com>
2023-02-19 13:42:24 +0300

    new examples

--- Example007_Magic/Program.cs
+++ Example007_Magic/Program.cs
@@ -1,19 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.Clear();
-Console.Set.Cursor.Position(10,4);
-Console.WriteLine("+");
+﻿Console.Clear();
+//Console.SetCursorPosition(10,4);
+//Console.WriteLine("+");
 
 int xa = 1;
 int ya = 1;
 int xb = 1;
 int yb = 30;
-int xc = 40;
+int xc = 80;
 int yc = 30;
-Console.Set.Cursor.Position(xa,ya);
-Console.WriteLine ("+");
 
-Console.Set.Cursor.Position(xb,yb);
+Console.SetCursorPosition(xa, ya);
 Console.WriteLine ("+");
-
-Console.Set.Cursor.Position(xc,yc);
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine ("+");
+Console.SetCursorPosition(xc, yc);
 Console.WriteLine ("+");
---
+++ Example12_Methods/Program.cs
@@ -0,0 +1,45 @@
+﻿// void Method1()
+// {
+//  Console.WriteLine ("Author: ");
+// }
+// Method1();
+
+//void Method2(string msg)
+//{
+//Console.WriteLine(msg);
+//}
+//("Текст сообщения: ");
+
+//void Method2(string msg, int count)
+//{
+//int i = 0;
+//while (i<count)
+//{
+// Console.WriteLine(msg);
+//i++;
+//}
+
+//}
+//("Текст сообщения: ", 4);
+
+//int Method3() => DateTime.Now.Year;
+//int Year = Method3();
+//Console.WriteLine(Year);
+
+
+string Method4(int count, string text)
+
+    {
+         int i = 0;
+            string result = string.Empty;
+            
+            while (i < count)
+            {
+                result = result + text;
+                i++;
+            }
+
+            return result;
+        }
+        string res = Method4(10, "h");
+        Console.WriteLine(res);
