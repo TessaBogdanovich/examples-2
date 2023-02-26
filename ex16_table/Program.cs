2469941597d9f4cbfd361765b1392029b15c1f9d
Tessa Bogdanovich <surinastacey@gmail.com>
2023-02-26 14:09:11 +0300

    new eamples

---
+++ ex16_table/Program.cs
@@ -0,0 +1,25 @@
+﻿string[,] table = new string[2, 5];
+table[1, 2] = "word";
+// string.Empty
+// table[0,0] table[0,1] table[0,2] table[0,3] table[0,4]
+// table[1,0] table[1,1] table[1,2] table[1,3] table[1,4]
+
+for (int rows = 0; rows < 2; rows++)
+{
+    for (int columns = 0; columns < 5; columns++)
+    {
+        System.Console.WriteLine($"-{table[rows, columns]}-");
+    }
+}
+
+int[,] matrix = new int[3,4];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        System.Console.Write($"{matrix[i, j]} ");
+    }
+    System.Console.WriteLine();
+}
+
